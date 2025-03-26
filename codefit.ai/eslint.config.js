706b4ea4ed99c9e5e1f0eaa9afad7501a61c14d3
Brion <info@brionmario.com>
2025-03-26 16:08:11 +0530

    chore: add formatting support

--- codefit.ai/eslint.config.js
+++ codefit.ai/eslint.config.js
@@ -1,10 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
-  { ignores: ['dist'] },
+  eslintConfigPrettier,
+  {ignores: ['dist']},
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -12,7 +14,7 @@ export default [
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: {jsx: true},
         sourceType: 'module',
       },
     },
@@ -23,11 +25,8 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'no-unused-vars': ['error', {varsIgnorePattern: '^[A-Z_]'}],
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     },
   },
-]
+];
